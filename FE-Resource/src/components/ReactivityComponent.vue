70ad06c5be3428a57c34eaa61f7b4ab3490fdab9
Tru Nguyen <nguyentruhn@gmail.com>
2025-11-29 10:41:26 +0700

    add to cart

--- FE-Resource/src/components/ReactivityComponent.vue
+++ FE-Resource/src/components/ReactivityComponent.vue
@@ -1,133 +1,144 @@
 <script setup>
-import { computed, reactive, ref, watch } from 'vue'
-// ref
-const count = ref(0)
-watch(count, (newVal, oldVal) => {
-  console.log('Changed:', oldVal, '=>', newVal)
-})
-function increase() {
-  count.value++
-}
-// reactive
-const user = reactive({
-  name: 'Tru',
-  age: '27',
-  address: 'HN',
-})
+import { computed, onMounted, reactive, ref, watch, watchEffect } from 'vue'
 
-function updateCity() {
-    user.address = 'Ha Noi'
-}
-// v-bind
-const input = ref('')
-
-// computed() add to cart
+// danh sách sản phẩm
 const listProduct = reactive([
   {
-    id:1,
-    name:'áo phông',
+    id: 1,
+    name: 'áo phông',
     price: 100000,
-    quantity : 2
+    quantity: 4,
   },
   {
-    id:2,
-    name:'áo sơ mi',
-    price: 230000,
-    quantity : 6
+    id: 2,
+    name: 'áo sơ mi',
+    price: 250000,
+    quantity: 3,
   },
   {
-    id:3,
-    name:'Quần short',
-    price: 150000,
-    quantity : 4
-  }
+    id: 3,
+    name: 'áo hodie',
+    price: 100000,
+    quantity: 7,
+  },
+  {
+    id: 4,
+    name: 'quần âu',
+    price: 100000,
+    quantity: 4,
+  },
 ])
-const cart = reactive(
-    {
-      item:[
-        {
-          id:null,
-          name:null,
-          price:null,
-          quantity:null,
-        }
-      ],
-      totalQuantity:0
+// giỏ hàng - reactive()
+const carts = reactive(
+  {
+    items : [],
+    totalQuantity : 0,
+  }
+)
+// tự động tính tổng tiền hàng trong giỏ - computed()
+const totalPrice = computed(() => {
+  return carts.items.reduce((sum,currentValue) => sum + currentValue.price * currentValue.quantity,0)
+})
+// tự động serch
+const keySearch = ref('')
+const filter = computed(
+  ()=>{
+    const product = listProduct.filter(item=>item.name == keySearch.value)
+    console.log(keySearch.value);
+    return product;
+  }
+)
+// theo dõi giỏ hàng lưu vào locale storage -  watch()
+watch(
+  ()=>carts,
+  (newItem)=>{
+    localStorage.setItem('my-cart',JSON.stringify(newItem))
+    console.log('Đã lưu giỏ hàng!',newItem)
+  },
+  { deep: true }
+)
+// luôn chạy lần đầu, lần tiếp theo khi biến xảy ra phản ứng
+watchEffect(
+  ()=>carts.items,
+  ()=>{
+    const cartLocale = localStorage.getItem('my-cart');
+    if(cartLocale) {
+      const data = JSON.parse(cartLocale);
+      carts.items = data.items;
+      carts.totalQuantity = cartLocale.totalQuantity
     }
-  );
-const totalPrice = computed(() =>
-  cart.item.reduce((sum, p) => sum + p.price * p.quantity, 0)
-);
-
-function addToCart(product) {
-  const exists = cart.item.find(p => p.id === product.id);
-  const productAdd = listProduct.find(p => p.id === product.id);
-  
-  if(productAdd) {
-    productAdd.quantity--
-  }
-  if(productAdd.quantity <= 0 ) {
-    alert('hết hàng')
-    return;
-  }
-
-  if(exists) {
-    exists.quantity++
-    cart.totalQuantity++
-  }else {
-    cart.item.push({
-      id: product.id,
-      name: product.name,
-      price: product.price,
-      quantity: 1,
-    });
-    cart.quantity++
+    
+  },
+)
+// thêm sản phẩm vào giỏ hàng - fn()
+function addToCart(id) {
+  const product = listProduct.find(pr => id === pr.id)
+  const checkExist = carts.items.find(pr => id === pr.id )
+  if(checkExist) {
+    checkExist.quantity++
+    carts.totalQuantity++
+    product.quantity--
+  }else{
+    carts.items.push({
+      id:product.id,
+      name:product.name,
+      price:product.price,
+      quantity:1,
+    })
+    carts.totalQuantity++
+    product.quantity--
   }
 }
 
-const age = ref(18)
-const msg = computed(() => {
-  return age.value >=20 ?'quá tuổi':'dưới 20'
-})
 
-function updateAge() {
-  age.value +=1
+function deleteCart() {
+  carts.items = [];
+  carts.totalQuantity = 0;
+  localStorage.removeItem('my-cart')
 }
 </script>
 
 <template>
   <div>
-    <div>
-        <h3>--Danh sách sản phẩm--</h3>
-        <div class="list">
-            <div class="p-2" v-for="item in listProduct" :key="item.id">
-                <span class="border">tên sản phẩm: {{ item.name}}</span><br>
-                <span>Giá: {{ item.price}}</span><br>
-                <span>số lượng: {{ item.quantity}}</span><br>
-                <button class="border" @click="addToCart(item)">Add to cart</button>
-
-            </div>
-        </div>
-
-        <h3>--Cart--</h3>
-        <label for="">sản phẩm :</label>
-        <div class="" v-for = "product in cart.item" :key="product.id">
-          {{ product.name }} x {{ product.quantity}}
-        </div>
-        <div class=""><span>Tổng sản phẩm : {{ cart.totalQuantity }}</span></div>
-        <div class=""><span>Tổng tiền :{{ totalPrice }}</span></div>
-
-        <h1>tuổi : {{ age }}</h1>
-        <h1>{{ msg }}</h1>
+    <div class="list_product">
+      <h3>Danh sách sản phẩm</h3>
+      Tìm kiếm
+      <input type="text" class="border" v-model="keySearch">
+      <ul v-if="filter.length">
+        <li class="p-2" v-for="product in filter" :key="product.id">
+          <span class="p-2">{{ product.name }} - {{ product.price }}đ - sl:{{ product.quantity }}</span>
+          <button class="border p-1" @click="addToCart(product.id)">Add+</button>
+        </li>
+      </ul>
+      <ul v-if="filter.length === 0">
+        <li class="p-2" v-for="product in listProduct" :key="product.id">
+          <span class="p-2">{{ product.name }} - {{ product.price }}đ - sl:{{ product.quantity }}</span>
+          <button class="border p-1" @click="addToCart(product.id)">Add+</button>
+        </li>
+      </ul>
     </div>
 
-    <div class="mt-2">
-      <button @click="updateAge">updateAge()</button>
-    </div>
+    <div class="cart">
+      <h3>Cart</h3>
+      <ul>
+          <li v-for="pr in carts.items" :key="pr.id">
+            sp : {{ pr.name }} <span v-if="pr.name">x {{ pr.quantity }}</span>
+          </li>
+      </ul>
 
-  </div> 
+      <div class="total_price">
+       Tổng tiền : {{ totalPrice }}
+      </div>
+      <div class="total_quantity">
+       Tổng sản phẩm: {{carts.totalQuantity }}
+      </div>
+
+    <div class="">
+      <button class="border" @click="deleteCart">Xóa giỏ hàng</button>
+    </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
